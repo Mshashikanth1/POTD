87bb99d037774e9bf89a5c75da251c03b9d15396
shashikanth <mallepally.shashikanthh@yahoo.com>
2023-06-19 11:16:47 +0530

    Create Rearrange an array with O(1) extra space.java

---
+++ Rearrange an array with O(1) extra space.java
@@ -0,0 +1,61 @@
+
+//User function Template for Java
+/*
+Rearrange an array with O(1) extra space
+MediumAccuracy: 56.34%Submissions: 81K+Points: 4
+
+Given an array arr[] of size N where every element is in the range from 0 to n-1. Rearrange the given array so that the transformed array arrT[i] becomes arr[arr[i]].
+
+NOTE: arr and arrT are both same variables, representing the array before and after transformation respectively.
+
+Example 1:
+
+Input:
+N = 2
+arr[] = {1,0}
+Output: 0 1
+Explanation: 
+arr[arr[0]] = arr[1] = 0.
+arr[arr[1]] = arr[0] = 1.
+
+Example 2:
+
+Input:
+N = 5
+arr[] = {4,0,2,1,3}
+Output: 3 4 2 0 1
+Explanation: 
+arr[arr[0]] = arr[4] = 3.
+arr[arr[1]] = arr[0] = 4.
+and so on.
+
+Your Task:
+You don't need to read input or print anything. The task is to complete the function arrange() which takes arr and N as input parameters and rearranges the elements in the array in-place. 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= N <= 105
+0 <= Arr[i] < N
+
+*/
+
+class Solution
+{
+ 
+    // arr: input array
+    // n: size of array
+    //Function to rearrange an array so that arr[i] becomes arr[arr[i]]
+    //with O(1) extra space.
+    static void arrange(long arr[], int n)
+    {
+        // your code here
+        long[] brr=Arrays.copyOf(arr,n);
+        
+        for(int i=0;i<n;i++){
+            arr[i]=brr[Integer.valueOf(brr[i]+"")];
+        }
+        
+    }
+}
