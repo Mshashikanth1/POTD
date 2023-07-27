74efe86b58d314f4fc3cfd48cb8e7e6b8e2e3e66
shashikanth <mallepally.shashikanthh@yahoo.com>
2023-07-27 19:46:22 +0530

    Create Heap Sort.java

---
+++ Heap Sort.java
@@ -0,0 +1,92 @@
+ 
+class Solution {
+    //Function to build a Heap from array.
+    void buildHeap(int arr[], int n) {
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+    }
+
+    //Heapify function to maintain heap property.
+    void heapify(int arr[], int n, int i) {
+        int largest = i;
+        int leftChild = 2 * i + 1;
+        int rightChild = 2 * i + 2;
+
+        if (leftChild < n && arr[leftChild] > arr[largest]) {
+            largest = leftChild;
+        }
+
+        if (rightChild < n && arr[rightChild] > arr[largest]) {
+            largest = rightChild;
+        }
+
+        if (largest != i) {
+            // Swap the elements
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+
+    //Function to sort an array using Heap Sort.
+    public void heapSort(int arr[], int n) {
+        buildHeap(arr, n);
+
+        for (int i = n - 1; i > 0; i--) {
+            // Move current root (maximum element) to the end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Heapify the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+}
+
+/*
+Heap Sort
+MediumAccuracy: 53.06%Submissions: 91K+Points: 4
+POTD July Placement Special : All POTD in the month of July are based on popular interview questions. Solve every day to ace the upcoming Placement Season !
+
+Given an array of size N. The task is to sort the array elements by completing functions heapify() and buildHeap() which are used to implement Heap Sort.
+
+Example 1:
+
+Input:
+N = 5
+arr[] = {4,1,3,9,7}
+Output:
+1 3 4 7 9
+Explanation:
+After sorting elements
+using heap sort, elements will be
+in order as 1,3,4,7,9.
+
+Example 2:
+
+Input:
+N = 10
+arr[] = {10,9,8,7,6,5,4,3,2,1}
+Output:
+1 2 3 4 5 6 7 8 9 10
+Explanation:
+After sorting elements
+using heap sort, elements will be
+in order as 1, 2,3,4,5,6,7,8,9,10.
+
+Your Task :
+You don't have to read input or print anything. Your task is to complete the functions heapify(), buildheap() and heapSort() where heapSort() and buildheap() takes the array and it's size as input and heapify() takes the array, it's size and an index i as input. Complete and use these functions to sort the array using heap sort algorithm.
+Note: You don't have to return the sorted list. You need to sort the array "arr" in place.
+
+Expected Time Complexity: O(N * Log(N)).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 ≤ N ≤ 106
+1 ≤ arr[i] ≤ 106
+*/
