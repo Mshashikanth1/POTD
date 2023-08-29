93660594849832cf980a5e09384f44912ae8d330
shashikanth <mallepally.shashikanthh@yahoo.com>
2023-08-29 21:26:25 +0530

    Create Delete nodes having greater value on right.java

---
+++ Delete nodes having greater value on right.java
@@ -0,0 +1,77 @@
+
+class Solution
+{
+    Node compute(Node head)
+    {
+        // your code here
+        Node curr;
+        Node temp;
+        
+        if(head==null || head.next==null){
+            
+            return head;
+            
+        }
+        
+        
+        curr=head;
+        temp=compute(head.next);
+        
+       if(curr.data<temp.data){
+           
+           curr=temp;
+           head=curr;
+           
+       }else{
+           
+           curr.next=temp;
+           head=curr;
+           
+       }
+        return head;
+    }
+}
+/*
+Delete nodes having greater value on right
+MediumAccuracy: 35.51%Submissions: 115K+Points: 4
+Win from a prize pool of INR 15K and get exciting merch! Register your team for Hack-A-Thon today!
+
+Given a singly linked list, remove all the nodes in the list which have any node on their right whose value is greater. (Not just immidiate Right , but entire List on the Right)
+
+ 
+
+Example 1:
+
+Input:
+LinkedList = 12->15->10->11->5->6->2->3
+Output: 15 11 6 3
+Explanation: Since, 12, 10, 5 and 2 are
+the elements which have greater elements
+on the following nodes. So, after deleting
+them, the linked list would like be 15,
+11, 6, 3.
+
+Example 2:
+
+Input:
+LinkedList = 10->20->30->40->50->60
+Output: 60
+Explanation: All the nodes except the last
+node has a greater value node on its right,
+so all the nodes except the last node must
+be removed.
+
+Your Task:
+The task is to complete the function compute() which should modify the list as required and return the head of the modified linked list. The printing is done by the driver code,
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ size of linked list ≤ 105
+1 ≤ element of linked list ≤ 105
+Note: Try to solve the problem without using any extra space.
+*/
+
+
+
