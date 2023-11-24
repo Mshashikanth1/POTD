16342f844a461c27847c687e55c4eba3bc994233
shashikanth <mallepally.shashikanthh@yahoo.com>
2023-11-24 21:33:24 +0530

    Create Pascal Triangle.java

---
+++ Pascal Triangle.java
@@ -0,0 +1,57 @@
+
+// User function Template for Java
+
+class Solution {
+    ArrayList<Long> nthRowOfPascalTriangle(int n) {
+        // code here
+        ArrayList<Long> pre =new ArrayList<>();
+        long mod=1000000007;
+        for(int i=0;i<n;i++){
+            ArrayList<Long> current=new ArrayList<>();
+            for(int j=0;j<i+1;j++){
+                if(j==i | j==0) current.add(1L);
+                else { Long num=(pre.get(j)+pre.get(j-1)) % mod; current.add(num); }
+            }
+            pre=current;
+        }
+        return pre;
+    }
+}
+
+/*
+Pascal Triangle
+EasyAccuracy: 23.68%Submissions: 89K+Points: 2
+Black Friday Sale is LIVE!
+Get Flat 30% Off and Exclusive Cashbacks. Avail Limited Time Sale
+
+banner
+Given a positive integer N, return the Nth row of pascal's triangle.
+Pascal's triangle is a triangular array of the binomial coefficients formed by summing up the elements of previous row.
+The elements can be large so return it modulo 109 + 7.
+
+File:PascalTriangleAnimated2.gif
+
+Example 1:
+
+Input:
+N = 4
+Output: 
+1 3 3 1
+Explanation: 
+4th row of pascal's triangle is 1 3 3 1.
+Example 2:
+
+Input:
+N = 5
+Output: 
+1 4 6 4 1
+Explanation: 
+5th row of pascal's triangle is 1 4 6 4 1.
+Your Task:
+Complete the function nthRowOfPascalTriangle() which takes n, as input parameters and returns an array representing the answer. You don't to print answer or take inputs.
+
+Expected Time Complexity: O(N2)
+Expected Auxiliary Space: O(N2)
+
+Constraints:
+*/
