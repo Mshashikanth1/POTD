74b977fedf4b30e2a9102b947c9de3118ed96e17
Mallepally shashi kanth varma <mallepally.shashikanthh@yahoo.com>
2024-03-01 18:14:52 +0530

    Create Peak element.java

---
+++ Peak element.java
@@ -0,0 +1,75 @@
+
+/*Complete the function below*/
+
+class Solution
+{
+	// Function to find the peak element
+	// arr[]: input array
+	// n: size of array a[]
+	/* 
+	time : O(n)
+	space : O(1)
+	*/
+	public int peakElement(int[] arr,int n)
+    {
+       int index=0;
+       for(int i=0;i<n;i++){
+           if (  getLeftValue(arr,i) <=  arr[i] && getRightValue(arr,i) <= arr[i] ) {
+                index=i;
+                break;
+            }
+       }
+       return index;
+    }
+    
+    public int getLeftValue(int[] arr,int i){
+       return  i==0 ? Integer.MIN_VALUE : arr[i-1];
+    }
+     public int getRightValue(int[] arr,int i){
+       return  i==arr.length-1 ? Integer.MIN_VALUE : arr[i+1];
+    }
+}
+
+/*
+Peak element
+MediumAccuracy: 38.86%Submissions: 393K+Points: 4
+Internship Alert!
+New month-> Fresh Chance to top the leaderboard and get SDE Internship! 
+
+banner
+Given an 0-indexed array of integers arr[] of size n, find its peak element. An element is considered to be peak if it's value is greater than or equal to the values of its adjacent elements (if they exist).
+
+Note: The output will be 1 if the index returned by your function is correct; otherwise, it will be 0.
+
+Example 1:
+
+Input: 
+n = 3
+arr[] = {1, 2, 3}
+Peak element's index: 2
+Output: 
+1
+Explanation: 
+If the index returned is 2, then the output printed will be 1. Since arr[2] = 3 is greater than its adjacent elements, and there is no element after it, we can consider it as a peak element. No other index satisfies the same property.
+Example 2:
+
+Input:
+n = 7
+arr[] = {1, 1, 1, 2, 1, 1, 1}
+Output: 
+1
+Explanation: 
+In this case there are 5 peak elements with indices as {0,1,3,5,6}. Returning any of them will give you correct answer.
+Your Task:
+You don't have to read input or print anything. Complete the function peakElement() which takes the array arr[] and its size n as input parameters and return the index of the peak element.
+
+Expected Time Complexity: O( log(n) ).
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ n ≤ 105
+1 ≤ arr[i] ≤ 106
+
+
+
+*/
